2772e010556514d957f3c2171b8b53df7265fd87
Marios Ouranos <ouranosmarios@outlook.com.gr>
2023-03-12 10:30:27 +0200

    Initial commit

---
+++ src/main/java/com/example/store/store/model/Product.java
@@ -0,0 +1,15 @@
+package com.example.store.store.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+
+    private Integer productId;
+
+    private String productName;
+
+    private double productPrice;
+
+
+}
